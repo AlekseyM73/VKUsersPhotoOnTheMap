e8174ca851dec3ae51e0516da5fc74e5d1241f35
AlekseyM73 <pro.invader73@gmail.com>
2019-06-26 11:10:41 +0400

    Proceed

--- app/src/main/java/com/alekseyM73/MapActivity.java
+++ app/src/main/java/com/alekseyM73/MapActivity.java
@@ -1,77 +1,34 @@
 package com.alekseyM73;
-
-import android.graphics.Color;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMap.OnCircleClickListener;
 import com.google.android.gms.maps.GoogleMap.OnGroundOverlayClickListener;
-import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
-import com.google.android.gms.maps.GoogleMap.OnPolygonClickListener;
-import com.google.android.gms.maps.GoogleMap.OnPolylineClickListener;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.Circle;
-import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.GroundOverlay;
 import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.Polygon;
-import com.google.android.gms.maps.model.PolygonOptions;
-import com.google.android.gms.maps.model.Polyline;
-import com.google.android.gms.maps.model.PolylineOptions;
+
 
 public class MapActivity extends AppCompatActivity implements
-        OnCircleClickListener,
-        OnGroundOverlayClickListener,
-        OnMarkerClickListener,
-        OnMapAndViewReadyListener.OnGlobalLayoutAndMapReadyListener,
-        OnPolygonClickListener,
-        OnPolylineClickListener
+        OnMapReadyCallback,
+        LocationListener,
+        OnGroundOverlayClickListener {
 
-
-    {
-
-        private static final LatLng ADELAIDE = new LatLng(-34.92873, 138.59995);
-        private static final LatLng BRISBANE = new LatLng(-27.47093, 153.0235);
-        private static final LatLng DARWIN = new LatLng(-12.425892, 130.86327);
-        private static final LatLng HOBART = new LatLng(-42.8823388, 147.311042);
-        private static final LatLng PERTH = new LatLng(-31.952854, 115.857342);
-        private static final LatLng SYDNEY = new LatLng(-33.87365, 151.20689);
-
-        private static class CustomTag {
-            private final String description;
-            private int clickCount;
-
-            public CustomTag(String description) {
-                this.description = description;
-                clickCount = 0;
-            }
-
-            public void incrementClickCount() {
-                clickCount++;
-            }
-
-            @Override
-            public String toString() {
-                return "The " + description + " has been clicked " + clickCount + " times.";
-            }
-        }
-
+    private static final LatLng ULYANOVSK = new LatLng(54.1850, 48.2333);
         private GoogleMap mMap = null;
-
-        private Circle mAdelaideCircle;
         private GroundOverlay mSydneyGroundOverlay;
-        private Marker mHobartMarker;
-        private Polygon mDarwinPolygon;
-        private Polyline mPolyline;
-
-
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -80,129 +37,115 @@ public class MapActivity extends AppCompatActivity implements
 
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        new OnMapAndViewReadyListener(mapFragment, this);
+        mapFragment.getMapAsync( this);
     }
 
         @Override
         public void onMapReady(GoogleMap map) {
         mMap = map;
-
         UiSettings mUiSettings = mMap.getUiSettings();
 
-        // Turn off the map toolbar.
         mUiSettings.setMapToolbarEnabled(false);
+        mUiSettings.setMyLocationButtonEnabled(true);
+        mUiSettings.setZoomControlsEnabled(true);
+        mUiSettings.setScrollGesturesEnabled(true);
+        mUiSettings.setZoomGesturesEnabled(true);
 
-        // Disable interaction with the map - other than clicking.
-            /*
-        mUiSettings.setZoomControlsEnabled(false);
-        mUiSettings.setScrollGesturesEnabled(false);
-        mUiSettings.setZoomGesturesEnabled(false);
-        mUiSettings.setTiltGesturesEnabled(false);
-        mUiSettings.setRotateGesturesEnabled(false);
-*/
-        // Add a circle, a ground overlay, a marker, a polygon and a polyline to the map.
         addObjectsToMap();
 
-        // Set listeners for click events.  See the bottom of this class for their behavior.
-        mMap.setOnCircleClickListener(this);
         mMap.setOnGroundOverlayClickListener(this);
-        mMap.setOnMarkerClickListener(this);
-        mMap.setOnPolygonClickListener(this);
-        mMap.setOnPolylineClickListener(this);
 
-        // Override the default content description on the view, for accessibility mode.
-        // Ideally this string would be localised.
-
-
-        // Create bounds that include all locations of the map.
-        LatLngBounds bounds = new LatLngBounds.Builder()
-                .include(ADELAIDE)
-                .include(BRISBANE)
-                .include(DARWIN)
-                .include(HOBART)
-                .include(PERTH)
-                .include(SYDNEY)
-                .build();
-        mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 100));
+        showMyLocation();
     }
 
         private void addObjectsToMap() {
-        // A circle centered on Adelaide.
-        mAdelaideCircle = mMap.addCircle(new CircleOptions()
-                .center(ADELAIDE)
-                .radius(500000)
-                .fillColor(Color.argb(150, 66, 173, 244))
-                .strokeColor(Color.rgb(66, 173, 244))
-                .clickable(true));
-        mAdelaideCircle.setTag(new CustomTag("Adelaide circle"));
-
-        // A ground overlay at Sydney.
-        mSydneyGroundOverlay = mMap.addGroundOverlay(new GroundOverlayOptions()
+            mSydneyGroundOverlay = mMap.addGroundOverlay(new GroundOverlayOptions()
                 .image(BitmapDescriptorFactory.fromResource(R.drawable.harbour_bridge))
-                .position(SYDNEY, 700000)
+                .position(ULYANOVSK, 700000)
                 .clickable(true));
-        mSydneyGroundOverlay.setTag(new CustomTag("Sydney ground overlay"));
-
-        // A marker at Hobart.
-        mHobartMarker = mMap.addMarker(new MarkerOptions().position(HOBART));
-        mHobartMarker.setTag(new CustomTag("Hobart marker"));
-
-        // A polygon centered at Darwin.
-        mDarwinPolygon = mMap.addPolygon(new PolygonOptions()
-                .add(
-                        new LatLng(DARWIN.latitude + 3, DARWIN.longitude - 3),
-                        new LatLng(DARWIN.latitude + 3, DARWIN.longitude + 3),
-                        new LatLng(DARWIN.latitude - 3, DARWIN.longitude + 3),
-                        new LatLng(DARWIN.latitude - 3, DARWIN.longitude - 3))
-                .fillColor(Color.argb(150, 34, 173, 24))
-                .strokeColor(Color.rgb(34, 173, 24))
-                .clickable(true));
-        mDarwinPolygon.setTag(new CustomTag("Darwin polygon"));
-
-        // A polyline from Perth to Brisbane.
-        mPolyline = mMap.addPolyline(new PolylineOptions()
-                .add(PERTH, BRISBANE)
-                .color(Color.rgb(103, 24, 173))
-                .width(30)
-                .clickable(true));
-        mPolyline.setTag(new CustomTag("Perth to Brisbane polyline"));
     }
 
-        //
-        // Click event listeners.
-        //
+        private void showMyLocation() {
+            LocationManager locationManager = (LocationManager)
+                    getSystemService(LOCATION_SERVICE);
+            String locationProvider = this.getEnabledLocationProvider();
+            if (locationProvider == null) {
+                return;
+            }
 
-        private void onClick(CustomTag tag) {
-       // tag.incrementClickCount();
-       // mTagText.setText(tag.toString());
-    }
+            final long MIN_TIME_BW_UPDATES = 1000;
+            final float MIN_DISTANCE_CHANGE_FOR_UPDATES = 1;
 
-        @Override
-        public void onCircleClick(Circle circle) {
-        onClick((CustomTag) circle.getTag());
-    }
+            Location myLocation = null;
+            try {
+                locationManager.requestLocationUpdates(
+                        locationProvider,
+                        MIN_TIME_BW_UPDATES,
+                        MIN_DISTANCE_CHANGE_FOR_UPDATES, (LocationListener) this);
+                myLocation = locationManager
+                        .getLastKnownLocation(locationProvider);
+            }
+            catch (SecurityException e) {
+                Toast.makeText(this, "Show My Location Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                e.printStackTrace();
+                return;
+            }
+
+            if (myLocation != null) {
+                LatLng latLng = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 13));
+                CameraPosition cameraPosition = new CameraPosition.Builder()
+                        .target(latLng)             // Sets the center of the map to location user
+                        .zoom(15)                   // Sets the zoom
+                        .build();                   // Creates a CameraPosition from the builder
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                // Add Marker to Map
+                MarkerOptions option = new MarkerOptions();
+               // option.title("My Location");
+                option.snippet("....");
+                option.position(latLng);
+                Marker currentMarker = mMap.addMarker(option);
+
+            } else {
+                Toast.makeText(this, "Location not found!", Toast.LENGTH_LONG).show();
+            }
+
+        }
+        private String getEnabledLocationProvider() {
+                LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+                Criteria criteria = new Criteria();
+                String bestProvider = locationManager.getBestProvider(criteria, true);
+            boolean enabled = locationManager.isProviderEnabled(bestProvider);
+
+            if (!enabled) {
+                Toast.makeText(this, "No location provider enabled!", Toast.LENGTH_LONG).show();
+                return null;
+            }
+            return bestProvider;
+        }
 
         @Override
         public void onGroundOverlayClick(GroundOverlay groundOverlay) {
-        onClick((CustomTag) groundOverlay.getTag());
+            Toast.makeText(this,"pic",Toast.LENGTH_SHORT).show();
     }
 
         @Override
-        public boolean onMarkerClick(final Marker marker) {
-        onClick((CustomTag) marker.getTag());
-        // We return true to indicate that we have consumed the event and that we do not wish
-        // for the default behavior to occur (which is for the camera to move such that the
-        // marker is centered and for the marker's info window to open, if it has one).
-        return true;
-    }
+        public void onLocationChanged(Location location) {
+
+        }
 
         @Override
-        public void onPolygonClick(Polygon polygon) {
-        onClick((CustomTag) polygon.getTag());
-    }
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
 
         @Override
-        public void onPolylineClick(Polyline polyline) {
-        onClick((CustomTag) polyline.getTag());
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
     }
-}
